5eb7bdee6f5bc1de06a08c5dfd7e22d72a470a51
Kaique de Miranda <kbdemiranda@hotmail.com>
2024-01-27 16:20:09 -0300

    Lista de times

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,15 +1,28 @@
 import {CampoTexto} from "../CampoTexto";
+import {ListaSuspensa} from "../ListaSuspensa";
 
 import ('./style.css')
 
 
 export const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
   return (
       <section className="formulario">
           <form>
               <CampoTexto label="nome" placeholder="Digite seu nome" type="text"/>
               <CampoTexto label="cargo" placeholder="Digite seu cargo" type="text"/>
-              <CampoTexto label="imagem" placeholder="Digite o endereço da sua imagem" type="url"/>
+              <CampoTexto label="imagem" placeholder="Digite o URL da sua imagem" type="url"/>
+              <ListaSuspensa label="times" itens={times} />
           </form>
       </section>
   )
---
+++ src/components/ListaSuspensa/index.js
@@ -0,0 +1,14 @@
+import ('./style.css')
+
+export const ListaSuspensa = (props) => {
+  return(
+      <div className="lista-suspensa">
+          <label>{props.label}</label>
+          <select>
+              {props.itens.map(item => {
+                  return <option key={item}>{item}</option>
+              })}
+          </select>
+      </div>
+  )
+}
